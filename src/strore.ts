97e07e358427bd368491d297edfdc4b4368cb0d3
Edgard <edgard.joel4@gmail.com>
2024-04-17 17:28:00 -0500

    Se completo proyecto

---
+++ src/strore.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService";
+import { CryptoPrice, Cryptocurrency, Pair } from "./types";
+import { devtools } from "zustand/middleware";
+
+type CryptoStore = {
+  cryptocurrencies: Cryptocurrency[];
+  result: CryptoPrice;
+  loading: boolean;
+  fetchCryptos: () => Promise<void>;
+  fetchData: (pair: Pair) => Promise<void>;
+};
+
+export const useCryptoStore = create<CryptoStore>()(
+  devtools((set) => ({
+    cryptocurrencies: [],
+    result: {
+      IMAGEURL: "",
+      PRICE: "",
+      HIGHDAY: "",
+      LOWDAY: "",
+      CHANGEPCT24HOUR: "",
+      LASTUPDATE: "",
+    },
+    loading: false,
+    fetchCryptos: async () => {
+      const cryptocurrencies = await getCryptos();
+      set(() => ({
+        cryptocurrencies,
+      }));
+    },
+    fetchData: async (pair: Pair) => {
+      set(() => ({
+        loading: true,
+      }));
+
+      const result = await fetchCurrentCryptoPrice(pair);
+
+      set(() => ({
+        result,
+        loading: false,
+      }));
+    },
+  }))
+);
